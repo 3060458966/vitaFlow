4fd14eddd8a516d35fd3bbd7e684d04d540130ea
mageswaran1989 <mageswaran1989@gmail.com>
2019-05-27 10:47:43 +0530

    gin macro support tested

--- vitaflow/playground/conll2003/__init__.py
+++ vitaflow/playground/conll2003/__init__.py
@@ -1,3 +1,8 @@
+import gin
 from vitaflow.data_generators.text.conll_2003_dataset import CoNLL2003Dataset
 from vitaflow.iterators.text.csv_seq_to_seq_iterator import CSVSeqToSeqIterator
 from vitaflow.models.text.seq2seq.bilstm_crf import BiLSTMCrf
+
+@gin.configurable
+def get_experiment_root_directory(value):
+    return value
